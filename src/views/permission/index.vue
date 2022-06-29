7c5996bd2e813be173b30960341f0b644814bf4d
ZY <zy30651@qq.com>
2022-06-30 00:09:30 +0800

    权限刚开始

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -3,7 +3,7 @@
     <div class="app-container">
         <page-tools>
           <template v-slot:after>
-            <el-button type="primary" size="small" @click="addPermission(1)">添加权限</el-button>
+            <el-button type="primary" size="small" @click="addPermission(1, 0)">添加权限</el-button>
           </template>
         </page-tools>
 
@@ -13,19 +13,19 @@
           <el-table-column align='center' prop="description" label="描述"></el-table-column>
           <el-table-column label="操作">
             <template v-slot='{row}'>
-              <el-button v-if='row.type===1' type="text" @click="addPermission(2)">添加</el-button>
-              <el-button type="text" >编辑</el-button>
+              <el-button v-if='row.type===1' type="text" @click="addPermission(2, row.id)">添加</el-button>
+              <el-button type="text" @click="editPermission(row.id)">编辑</el-button>
               <el-button type="text" @click="delPermission(row.id)">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
     </div>
-    <el-dialog :visible='showDialog' title="新增编辑">
-      <el-form label-width='120px'>
-        <el-form-item label='名称'>
+    <el-dialog :visible='showDialog' title="新增编辑" @close="btnCancel">
+      <el-form ref="permForm" :model="formData" :rules="rules" label-width='120px'>
+        <el-form-item label='名称' prop="name">
           <el-input v-model='formData.name' style='width:90%'/>
         </el-form-item>
-        <el-form-item label='标识'>
+        <el-form-item label='标识' prop="code">
           <el-input v-model='formData.code' style='width:90%'/>
         </el-form-item>
         <el-form-item label='描述'>
@@ -35,14 +35,13 @@
           <el-switch
           v-model='formData.enVisible'
           active-value="1"
-          inactive-value="0"
-          style='width:90%'/>
+          inactive-value="0"/>
         </el-form-item>
       </el-form>
       <el-row slot="footer" type="flex" justify="center">
         <el-col :span=6>
-          <el-button type="primary" size="small">确定</el-button>
-          <el-button size="small">取消</el-button>
+          <el-button type="primary" size="small" @click="btnOK">确定</el-button>
+          <el-button size="small" @click="btnCancel">取消</el-button>
         </el-col>
       </el-row>
     </el-dialog>
@@ -64,6 +63,10 @@ export default {
         type: '', // 类型 该类型 不需要显示 因为点击添加的时候已经知道类型了
         pid: '', // 因为做的是树 需要知道添加到哪个节点下了
         enVisible: '0' // 开启
+      },
+      rules: {
+        name: [{ required: true, message: '权限名称不能为空', trigger: 'blur' }],
+        code: [{ required: true, message: '权限标识不能为空', trigger: 'blur' }]
       }
     }
   },
@@ -82,8 +85,43 @@ export default {
         this.getPermissionList()
       })
     },
-    addPermission(type) {
+    addPermission(type, pid) {
       // 访问权的type=1； 按钮操作权的type=2
+      // pid标识当前数据的父节点标识
+      this.formData.type = type
+      this.formData.pid = pid
+      this.showDialog = true
+    },
+    btnOK() {
+      // this.$refs.permForm.validate(isOK => {})
+      this.$refs.permForm.validate().then(() => {
+        // 校验成功进入then
+        // addPermission(this.formData).then(()=>{})
+        if (this.formData.id) {
+          return updatePermission(this.formData)
+        } else {
+          return addPermission(this.formData)
+        }
+      }).then(() => {
+        this.$message.success('操作成功')
+        this.getPermissionList()
+        this.showDialog = false
+      })
+    },
+    btnCancel() {
+      this.formData = {
+        name: '', // 名称
+        code: '', // 标识
+        description: '', // 描述
+        type: '', // 类型 该类型 不需要显示 因为点击添加的时候已经知道类型了
+        pid: '', // 因为做的是树 需要知道添加到哪个节点下了
+        enVisible: '0' // 开启
+      }
+      this.$refs.permForm.resetFields()
+      this.showDialog = false
+    },
+    async editPermission(id) {
+      this.formData = await getPermissionDetail(id)
       this.showDialog = true
     }
   }
